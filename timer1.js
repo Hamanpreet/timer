3f0b9cf2fde0c5c8755256d406505d7e7b7939d2
Hamanpreet <haman42626@gmail.com>
2023-05-11 23:58:17 -0400

    Implementing an alarm clock by taking Command Line Arguments from User with comments

--- timer1.js
+++ timer1.js
@@ -1,10 +1,9 @@
-let arr = process.argv.slice(2, process.argv.length);      //removing the first two elements from CLI to get the array we need
-
-for (let item of arr) {
-  const number = item * 1;                                // converting the string array elements to number for the checks
-  if (item > 0 && Number.isInteger(number)) {             // We are ignoring non-number and negative values this way
+const times = process.argv.slice(2, process.argv.length);      //removing the first two elements from process.argv array to get the Command Line arguments we need
+for (let time of times) {
+  const number = time * 1;                                // converting the string array elements to number for the checks
+  if (number > 0 && Number.isInteger(number)) {             // We are ignoring non-number and negative values this way
     setTimeout(() => {
       process.stdout.write('\x07');                       // setting the beep
-    }, item * 1000);                                      // conveting ms to seconds
+    }, time * 1000);                                      // conveting ms to seconds
   }
 }
